254bf2be0ecca1a6cc8de1983cab1bf54febd5f8
Jonathan Heilmann <mail@jonathan-heilmann.de>
2019-01-19 21:39:10 +0100

    app `switch-theme-toggle`: Use theme from themesProvider

--- src/app/pages/switch-theme-toggle/switch-theme-toggle.page.ts
+++ src/app/pages/switch-theme-toggle/switch-theme-toggle.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToggleThemes } from 'ionic-angular-theme-switch';
+import { vibrantTheme } from '../../providers/themes.provider';
 
 @Component({
   selector: 'app-switch-theme-toggle',
@@ -8,32 +9,9 @@ import { ToggleThemes } from 'ionic-angular-theme-switch';
 })
 export class SwitchThemeTogglePage {
 
-  // see https://github.com/ionic-team/ionic/blob/master/core/src/themes/test/css-variables/css/vibrant.css
   public themes: ToggleThemes = {
     default: {},
-    alternative: {
-      primary: '#ff7f50',
-      secondary: '#17deee',
-      tertiary: '#ff4162',
-      success: '#39ff14',
-      warning: '#ffce00',
-      danger: '#f04141',
-      light: '#f4f5f8',
-      medium: '#989aa2',
-      dark: '#222428',
-
-      'ion-background-color': '#778899',
-
-      /* Component Colors */
-      'ion-backdrop-color': '#556677',
-      'ion-overlay-background-color': '#667788',
-
-      'ion-border-color': '#5bff76',
-      'ion-box-shadow-color': '#000',
-
-      'ion-item-background': '#667788',
-      'ion-item-border-color': '#5bff76'
-    }
+    alternative: vibrantTheme
   };
 
 }
